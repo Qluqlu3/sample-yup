4891f8b8c1fdac5d48d7dc5d4a6161032db90851
Qluqlu3 <zveupmjsgv@gmail.com>
2021-07-19 23:34:46 +0900

    htmlファイルを表示する

--- golang/go-api-sample01/main.go
+++ golang/go-api-sample01/main.go
@@ -1,12 +1,21 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	app := gin.Default()
 	app.GET("/hello", func(content *gin.Context) {
 		content.JSON(200, gin.H{
 			"message": "world",
+		})
+	})
+	// htmlのディレクトリを指定
+	app.LoadHTMLGlob("html/*")
+	app.GET("/gin", func(content *gin.Context) {
+		content.HTML(200, "index.html", gin.H{
+			"message": "こんにちは",
 		})
 	})
 	app.Run(":5000")
