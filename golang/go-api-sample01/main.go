0a90690592115254e96255aa8d0bc640d4beaae6
Qluqlu3 <zveupmjsgv@gmail.com>
2021-07-22 01:10:07 +0900

    フォーマットで整形・ラジオボタンの情報取得

--- golang/go-api-sample01/main.go
+++ golang/go-api-sample01/main.go
@@ -28,7 +28,12 @@ func main() {
 	//POSTでデータ取得・表示
 	app.POST("/post", func(content *gin.Context) {
 		name := content.PostForm("name")
-		content.JSON(http.StatusOK, gin.H{"name": name,})
+		content.JSON(http.StatusOK, gin.H{"name": name})
+	})
+	app.POST("/form", func(content *gin.Context) {
+		text := content.PostForm("text")
+		sex := content.PostForm("sex")
+		content.JSON(http.StatusOK, gin.H{"text": text, "sex": sex})
 	})
 	app.Run(":5000")
 }
