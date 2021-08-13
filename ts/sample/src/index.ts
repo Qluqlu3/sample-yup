c97cd511b90f597f4e6004e683c74e061ba43cbb
kay <kay@kay.local>
2021-08-14 04:07:39 +0900

    　jest学習

--- ts/sample/src/index.ts
+++ ts/sample/src/index.ts
@@ -1,11 +1,20 @@
 export class Sample {
   private name: string;
-  public price: number;
-  constructor(name: string, price: number) {
+  // public price: number;
+
+  constructor(name: string) {
     this.name = name;
-    this.price = price;
+    // this.price = price;
   }
+
   sendMessage() {
     return this.name;
+  }
+
+  checkFizzBuzz(num: number) {
+    if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
+    else if (num % 3 === 0) return 'Fizz';
+    else if (num % 5 === 0) return 'Buzz';
+    else return '';
   }
 }
--- ts/sample/test/index.test.ts
+++ ts/sample/test/index.test.ts
@@ -1,7 +1,17 @@
 import { Sample } from '../src/index';
 
-describe('greet', () => {
-  test('should say hello to Tom.', () => {
+describe('sendMessage', () => {
+  test('sendMessage', () => {
     expect(new Sample('name').sendMessage()).toBe('name');
+  });
+});
+
+describe('FizzBuzz', () => {
+  test('FizzBuzz', () => {
+    const sample = new Sample('name');
+    expect(sample.checkFizzBuzz(3)).toBe('Fizz');
+    expect(sample.checkFizzBuzz(5)).toBe('Buzz');
+    expect(sample.checkFizzBuzz(15)).toBe('FizzBuzz');
+    expect(sample.checkFizzBuzz(17)).toBe('');
   });
 });
