e838c35d678f5da6690f217d4cd4f2e457186aa2
kay <kay@kay.local>
2021-08-18 00:02:07 +0900

    母音削除

--- ts/test-ts/handleName.ts
+++ ts/test-ts/handleName.ts
@@ -2,7 +2,7 @@ const deleteVowels = (value: string): string => {
   let result = '';
   let target = value.split('');
   for (let i = 0; i < value.length; i++) {
-    result += value[i].replace(/[aiueoAIUEO]/, '');
+    result += target[i].replace(/[aiueoAIUEO]/, '');
   }
   return result;
 };
