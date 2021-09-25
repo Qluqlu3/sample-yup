c8d90fa111d41416135ca4ba1aace75f7e3d3498
Qluqlu3 <plksmcgof@gmail.com>
2021-09-25 23:36:14 +0900

    数字検索

--- ts/test-ts/src/searchDislikeNumber.ts
+++ ts/test-ts/src/searchDislikeNumber.ts
@@ -1,11 +1,12 @@
-const searchDislikeNumber = (value: number, target: string) => {
-  if (/[value]/.test(target)) {
-    console.log('なし');
-  } else {
+const searchDislikeNumber = (value: string, target: string) => {
+  const reg = new RegExp(value);
+  if (reg.test(target)) {
     console.log('あり');
+  } else {
+    console.log('なし');
   }
 };
 
-console.log(searchDislikeNumber(4, '12345'));
+console.log(searchDislikeNumber('4', '45759'));
 
-console.log(searchDislikeNumber(4, '93210'));
+console.log(searchDislikeNumber('4', '93210'));
