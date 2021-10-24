80273ffb984a44f33bc410cac7b64f2669a7bfc7
Qluqlu3 <plksmcgof@gmail.com>
2021-10-24 22:58:38 +0900

    hook form number

--- react/hook-form/src/App.tsx
+++ react/hook-form/src/App.tsx
@@ -13,8 +13,11 @@ export const App: React.FC<Props> = () => {
     mode: 'onChange',
     defaultValues: {
       name: '',
+      age: 0,
     },
   });
+
+  const numberRegExp = /^[0-9]+$/;
 
   const onSubmit = handleSubmit((data: FormState) => {
     console.log(data);
@@ -34,6 +37,28 @@ export const App: React.FC<Props> = () => {
               maxLength: {
                 value: 10,
                 message: '20文字以内',
+              },
+            })}
+          />
+          <label>年齢</label>
+          <input
+            type='number'
+            name='age'
+            placeholder='年齢'
+            ref={register({
+              valueAsNumber: true,
+              required: '必須項目',
+              pattern: {
+                value: numberRegExp,
+                message: '整数で入力',
+              },
+              min: {
+                value: 0,
+                message: '0以上の数字',
+              },
+              max: {
+                value: 130,
+                message: '130以下の数字',
               },
             })}
           />
